a51ee5470bf97513eb428654773164227979693f
yeob <rlatkdduq1234@gmail.com>
2025-11-24 22:52:48 +0900

    feat : add-subtractfunc
    #1

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from typing import Union
 def add(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
     return x + y
 
+def subtract(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+    return x - y
+
 if __name__ == "__main__":
     print(add(1, 2))
+    print(subtract(1, 2))
